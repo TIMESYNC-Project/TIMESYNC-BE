643b3f928c439bf3c0074e382ed7c35e30667aa1
Fauzi Sofyan <fauzilax@gmail.com>
2023-02-05 21:27:20 +0700

    remove duplicate import user package

--- config/db.go
+++ config/db.go
@@ -4,11 +4,9 @@ import (
 	"fmt"
 	"log"
 	aData "timesync-be/features/announcement/data"
-	uData "timesync-be/features/user/data"
-
 	attendData "timesync-be/features/attendance/data"
 	settData "timesync-be/features/setting/data"
-	usrData "timesync-be/features/user/data"
+	uData "timesync-be/features/user/data"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -29,7 +27,6 @@ func InitDB(dc DBConfig) *gorm.DB {
 func Migrate(db *gorm.DB) {
 	db.AutoMigrate(uData.User{})
 	db.AutoMigrate(aData.Announcement{})
-	db.AutoMigrate(usrData.User{})
 	db.AutoMigrate(attendData.Attendance{})
 	db.AutoMigrate(settData.Setting{})
 }
